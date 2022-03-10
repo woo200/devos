1f647ca3d46753b6dbfa548151f3b5295e2cfffe
licyb200 <licyb200@protonmail.com>
2022-03-09 17:33:29 -0800

    64 bit Push ALL Pop ALL macro added

--- src/kernel/interrupts/ISR1.asm
+++ src/kernel/interrupts/ISR1.asm
@@ -1,6 +1,26 @@
 global isr1
 global LoadIDT
 
+%macro PUSHALL 0
+    push rax
+    push rcx
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+%endmacro
+
+%macro POPALL 0
+    push r11
+    push r10
+    push r9
+    push r8
+    push rdx
+    push rcx
+    push rax
+%endmacro
+
 [extern _idt]
 idtDescriptor:
     dw 4095
@@ -8,10 +28,10 @@ idtDescriptor:
 
 [extern isr1_handler]
 isr1:
-    pushad
+    PUSHALL
     cld
     call isr1_handler
-    popad
+    POPALL
     iretq
 
 LoadIDT:
